77672ae9d82455c44ee88076c55e081dd05f1df7
hdensten <hannah.densten@gmail.com>
2019-10-29 17:28:07 -0600

    Auth state & private routes

--- leadmanager/frontend/src/reducers/index.js
+++ leadmanager/frontend/src/reducers/index.js
@@ -2,9 +2,11 @@ import { combineReducers } from "redux";
 import movies from "./movies";
 import errors from "./errors";
 import messages from "./messages";
+import auth from "./auth";
 
 export default combineReducers({
   movies,
   errors,
-  messages
+  messages,
+  auth
 });
