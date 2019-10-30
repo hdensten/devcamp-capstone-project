34c8867d4277a7b703ea21574c35993a34193ad1
hdensten <hannah.densten@gmail.com>
2019-10-29 20:46:28 -0600

    frontend authentication

--- leadmanager/frontend/src/actions/movies.js
+++ leadmanager/frontend/src/actions/movies.js
@@ -1,12 +1,13 @@
 import axios from "axios";
 import { createMessage, returnErrors } from "./messages";
+import { tokenConfig } from "./auth";
 
-import { GET_MOVIES, DELETE_MOVIE, ADD_MOVIE, GET_ERRORS } from "./types";
+import { GET_MOVIES, DELETE_MOVIE, ADD_MOVIE } from "./types";
 
 // GET MOVIES
-export const getMovies = () => dispatch => {
+export const getMovies = () => (dispatch, getState) => {
   axios
-    .get("/api/movies/")
+    .get("/api/movies/", tokenConfig(getState))
     .then(res => {
       dispatch({
         type: GET_MOVIES,
@@ -19,9 +20,9 @@ export const getMovies = () => dispatch => {
 };
 
 // DELETE MOVIE
-export const deleteMovie = id => dispatch => {
+export const deleteMovie = id => (dispatch, getState) => {
   axios
-    .delete(`/api/movies/${id}/`)
+    .delete(`/api/movies/${id}/`, tokenConfig(getState))
     .then(res => {
       dispatch(createMessage({ deleteMovie: "Movie Deleted" }));
       dispatch({
@@ -33,9 +34,9 @@ export const deleteMovie = id => dispatch => {
 };
 
 // ADD MOVIE
-export const addMovie = movie => dispatch => {
+export const addMovie = movie => (dispatch, getState) => {
   axios
-    .post("/api/movies/", movie)
+    .post("/api/movies/", movie, tokenConfig(getState))
     .then(res => {
       dispatch(createMessage({ addMovie: "Movie Added" }));
       dispatch({
--- leadmanager/frontend/src/components/layout/Alerts.js
+++ leadmanager/frontend/src/components/layout/Alerts.js
@@ -15,11 +15,15 @@ export class Alerts extends Component {
       if (error.msg.email) alert.error(`Email: ${error.msg.email.join()}`);
       if (error.msg.message)
         alert.error(`Message: ${error.msg.message.join()}`);
+      if (error.msg.non_field_errors)
+        alert.error(error.msg.non_field_errors.join());
+      if (error.msg.username) alert.error(error.msg.username.join());
     }
 
     if (message !== prevProps.message) {
       if (message.deleteMovie) alert.success(message.deleteMovie);
       if (message.addMovie) alert.success(message.addMovie);
+      if (message.passwordNotMatch) alert.error(message.passwordNotMatch);
     }
   }
 
