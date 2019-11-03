971af286d44be7db1da5502a7bc33af231b8ab33
hdensten <hannah.densten@gmail.com>
2019-11-02 20:27:20 -0600

    Changed rating field type

--- leadmanager/movies/models.py
+++ leadmanager/movies/models.py
@@ -6,7 +6,7 @@ class Movie(models.Model):
     movie_id = models.IntegerField()
     title = models.CharField(max_length=100, blank=False)
     watch_date = models.DateField(auto_now=False, auto_now_add=False)
-    rating = models.DecimalField(max_digits=2, decimal_places=1)
+    rating = models.PositiveIntegerField(blank=False)
     review = models.CharField(max_length=5000, blank=True)
     owner = models.ForeignKey(
         User, related_name="movies", on_delete=models.CASCADE, null=True)
